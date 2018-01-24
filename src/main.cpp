e860846a2811e2b034e1b82a659142223f0238c5
eurekabitcoin <35718052+eurekabitcoin@users.noreply.github.com>
2018-01-24 06:10:05 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1090,7 +1090,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 50 * COIN;
 
     if (nHeight == 2)
-        nSubsidy = 19200000;
+        nSubsidy = 19200000 * COIN;
 
     // Subsidy is cut in half every 240000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 240000); // Eurekabitcoin: 840k blocks in ~4 years
